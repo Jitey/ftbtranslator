26978f0ea611e0cc07ef9331dfad6c3f3b751edb
Jitey <jt.bousseau@gmail.com>
2026-03-24 16:31:59 +0100

    Importing worspace module
    Convert the json workspace file into folders as the kubejs on in the modpack files

---
+++ mth/wimporter.py
@@ -0,0 +1,91 @@
+from pathlib import Path
+import json
+
+
+
+
+
+class WorkspaceImporter:
+    
+    def __init__(self, source_path: Path | str, target_path: Path | str) -> None:
+        self.source_path = Path(source_path)
+        self.target_path = Path(target_path)
+
+    @staticmethod
+    def open_json(path: str | Path) -> dict:
+        with open(path, "r") as f:
+            return json.load(f)
+        
+    @staticmethod
+    def write_json(content: dict, path: str | Path) -> None:
+        with open(path, "w") as f:
+            json.dump(content, f, indent=4)
+
+    @staticmethod
+    def create_directory(path: Path, dir_name: str="") -> Path:
+            """
+            Create a directory structure based on the test name and date.
+
+            Args:
+                base_path (str): The base path where the directory structure will be created.
+                dir_name (str): The name of the directory. 
+                date (dt): The date of the test.
+
+            Returns:
+                Path: The path to the created directory structure.
+            """
+            dir = Path(path) / dir_name if dir_name else path
+            
+            try:
+                dir.mkdir(parents=True, exist_ok=False)
+                print(f"> Directory created: {dir}")
+                
+            except FileExistsError:
+                print(f"[!] Directory already exists: {dir}")
+
+            return dir
+        
+    def create_file(self, content: dict) -> Path:
+        """Create a the corresponding json langage file and return his directory location
+
+        Args:
+            content (dict): File data dict
+
+        Returns:
+            Path: Directory path
+        """
+        file_path: str = content['path']
+        dir_path = self.target_path / Path(file_path).parent
+
+        try:
+            self.write_json(content['content'], file_path)
+            
+        except FileNotFoundError:
+            self.create_directory(dir_path)
+            self.write_json(content['content'], file_path)
+        
+        return dir_path
+
+    def import_from_workspace(self, file_name: str) -> None:
+        """Import Minecraft Translation Helper workspace from the generated json file and rebuild original kubejs directory
+
+        Args:
+            file_name (str): Name of the generated .json to import
+        """
+        file_path = self.source_path / f"{file_name}.json"
+        data = self.open_json(file_path)
+
+        for file in data['files']:
+            self.create_file(file)
+            
+        
+
+
+
+
+
+if __name__ == '__main__':
+    SOURCE_PATH = Path(__file__).resolve().parent.parent / "translations" / "imported"
+    TARGET_PATH = Path(__file__).resolve().parent.parent / "translations" / "old"
+
+    WorkspaceImporter(SOURCE_PATH, TARGET_PATH).import_from_workspace("StarT-Dev-Team_Star-Technology-workspace")
